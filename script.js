a82330df0f5b78872729be0ebac867c2e1723fa1
Anthony Sellers <asell0808@yahoo.com>
2021-09-14 21:03:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -43,13 +43,31 @@ const fun = (a,
 }
 fun(1,2,3,4,);
 
+// next example
 
+Object.values
+Object.entries
+Object.keys
 
+let obj = {
+    username0: 'Santa',
+    username1: 'Rudolph',
+    username2: 'Mr.Grinch'
+}
 
+Object.keys(obj).forEach((key, index) =>{
+    console.log(key, obj[key]);
+})
 
+Object.values(obj).forEach(value =>{
+    console.log(value);
+})
 
+Object.entries(obj).forEach(value =>{
+    console.log(value);
+})
 
-
-
-
+Object.entries(obj).map(value =>{
+    return value[1] + value[0].replace('username', '');
+})
 
